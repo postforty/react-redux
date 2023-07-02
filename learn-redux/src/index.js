09ef9a3b1c99c603416bc11333e38ea17cbbec91
postforty <75767073+postforty@users.noreply.github.com>
2023-07-02 23:50:37 +0900

    [ADD] 리덕스 모듈 생성, 루트 리듀서 생성, 리덕스 적용

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -4,11 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // import "./exercise";
+import rootReducer from "./modules";
+import { legacy_createStore as createStore } from "redux";
+import { Provider } from "react-redux";
 
+const store = createStore(rootReducer);
+console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
