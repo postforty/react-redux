1eb26b2e26345839d8b10b9f17de546262ffcd3f
postforty <75767073+postforty@users.noreply.github.com>
2023-07-03 20:11:54 +0900

    [ADD] 리덕스 개발자도구 적용

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -7,8 +7,11 @@ import reportWebVitals from "./reportWebVitals";
 import rootReducer from "./modules";
 import { legacy_createStore as createStore } from "redux";
 import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension"; // 리덕스 개발자 도구
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만듭니다.
+// composeWithDevTools 를 사용하여 리덕스 개발자 도구 활성화
+
 console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
