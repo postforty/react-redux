e3792c2e69875d44d85ab59e1e73663d076ef295
Dandy Code! <postforty@naver.com>
2023-07-10 13:02:31 +0900

    [UPDATE] API 재로딩 문제 해

--- learn-redux-middleware/src/containers/PostListContainer.js
+++ learn-redux-middleware/src/containers/PostListContainer.js
@@ -6,16 +6,16 @@ import { getPosts } from "../modules/posts";
 function PostListContainer() {
   const { data, loading, error } = useSelector((state) => state.posts.posts);
   const dispatch = useDispatch();
-  console.log("PostList : ", data);
 
   // 컴포넌트 마운트 후 포스트 목록 요청
   useEffect(() => {
     dispatch(getPosts());
   }, [dispatch]);
 
-  if (loading) return <div>로딩중...</div>;
+  if (loading && !data) return <div>로딩중...</div>; // 로딩중이면서, 데이터가 없을 때에만 로딩중... 표시
   if (error) return <div>에러 발생!</div>;
   if (!data) return null;
+
   return <PostList posts={data} />;
 }
 
